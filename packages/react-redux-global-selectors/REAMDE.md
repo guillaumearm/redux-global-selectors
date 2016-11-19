5b2a0c19f58d0e0a4d8f1505c672b95cf24cb327
Guillaume ARM <guillaume.arm@redpelicans.com>
2016-11-20 00:32:03 +0100

    Init packages/react-redux-global-selectors module
    import all files from react-redux-steps initial commit and :
      - replace all react-redux-steps occurences by
        react-redux-global-selectors
      - change README.md according to this changes
      - add keywords in package.json

---
+++ packages/react-redux-global-selectors/REAMDE.md
@@ -0,0 +1,3 @@
+# react-redux-global-selectors
+
+WIP
---
+++ packages/react-redux-global-selectors/__tests__/index.js
@@ -0,0 +1,10 @@
+import * as index from '../src';
+
+/* eslint-disable no-console */
+describe('react-redux-global-selectors', () => {
+    describe('exports', () => {
+        it('have a src/index.js file', () => {
+            expect(index).toBeDefined();
+        });
+    });
+});
